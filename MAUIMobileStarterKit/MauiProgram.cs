a36c8d6174f49ea68b52ed8e2dd06e603da10166
Gayan Perera <mgdperera@gmail.com>
2024-02-27 06:03:57 +0530

    add project

---
+++ MAUIMobileStarterKit/MauiProgram.cs
@@ -0,0 +1,55 @@
+﻿using Controls.UserDialogs.Maui;
+using MAUIMobileStarterKit.Interface;
+using MAUIMobileStarterKit.Screens;
+using MAUIMobileStarterKit.Utilities;
+using MAUIMobileStarterKit.ViewModels;
+using Microsoft.Extensions.Logging;
+
+namespace MAUIMobileStarterKit;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+            .UseUserDialogs(true, () =>
+            {
+#if ANDROID
+                var fontFamily = "OpenSans-Default.ttf";
+#else
+                    var fontFamily = "OpenSans-Regular";
+#endif
+                AlertConfig.DefaultMessageFontFamily = fontFamily;
+                AlertConfig.DefaultUserInterfaceStyle = UserInterfaceStyle.Dark;
+                AlertConfig.DefaultPositiveButtonTextColor = Colors.Purple;
+                ConfirmConfig.DefaultMessageFontFamily = fontFamily;
+                ActionSheetConfig.DefaultMessageFontFamily = fontFamily;
+                ToastConfig.DefaultMessageFontFamily = fontFamily;
+                SnackbarConfig.DefaultMessageFontFamily = fontFamily;
+                HudDialogConfig.DefaultMessageFontFamily = fontFamily;
+            })
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+        //Views
+        builder.Services.AddTransient<MainPage>();
+
+        //ViewModels
+        builder.Services.AddTransient<MainPageViewModels>();
+
+        //Services
+        builder.Services.AddSingleton<ILoading, Loading>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
+
